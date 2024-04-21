30a19a977b2e736885534d5fbabadb8027925dad
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2024-04-21 18:24:05 +0300

    adds blog card code

---
+++ app/components/BlogCard/BlogCard.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import React from 'react';
+
+const BlogCard = () => {
+  return (
+    <li className='blog-card max-w-[320px] p-6 rounded-[10px] shadow-card-shadow bg-custom-white md:min-w-[385px]'>
+      <div className='mb-6'>
+        <Image
+          src='illustration-article.svg'
+          alt='html open tag'
+          width='279'
+          height='200'
+          className='rounded-[10px] h-full w-full'
+        />
+      </div>
+      <div className='bg-yellow-bg font-extrabold text-xs py-1 px-3 inline-block rounded-[4px] mb-3'>
+        <p>Learning</p>
+      </div>
+      <p className='text-xs mb-3 md:text-sm'>Published 21 Dec 2023</p>
+      <h2 className='font-extrabold text-xl mb-3 md:text-2xl'>
+        HTML & CSS foundations
+      </h2>
+      <p className='text-sm text-custom-gray mb-3 md:text-base'>
+        These languages are the backbone of every website, defining structure,
+        content, and presentation.
+      </p>
+      <div className='flex items-center gap-2'>
+        <Image
+          src='/image-avatar.webp'
+          alt='Greg Hooper avatar'
+          width='32'
+          height='32'
+          className='rounded-full'
+        />
+        <p className='font-bold text-sm'>Greg Hooper</p>
+      </div>
+    </li>
+  );
+};
+
+export default BlogCard;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      boxShadow: {
+        'card-shadow': '8px 8px #000000',
+      },
+      colors: {
+        'custom-gray': '#7F7F7F',
+      },
+      backgroundColor: {
+        'yellow-bg': 'hsl(47, 88%, 63%)',
+        'custom-white': 'hsl(0, 0%, 100%)',
+        'custom-black': 'hsl(0, 0%, 7%)',
+        
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'card-shadow': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
